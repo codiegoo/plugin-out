5cf4385049ce6c54aa348dea8e2967186ab59266
Diego Flores Alcantar <diegoedvflores03@gmail.com>
2025-05-06 15:36:29 -0600

    changes

--- src/app/api/docs/route.js
+++ src/app/api/docs/route.js
@@ -2,6 +2,8 @@
 import { swaggerSpec } from '@/lib/swagger'
 
 export async function GET() {
+  // Verificar que swaggerSpec contiene las rutas correctamente
+  console.log(swaggerSpec);
   return new Response(JSON.stringify(swaggerSpec), {
     status: 200,
     headers: {
