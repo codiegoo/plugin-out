7fa1b48d6dcb3d31c9a9aa5b94a8ef40ef6553df
Diego Flores Alcantar <diegoedvflores03@gmail.com>
2025-05-06 15:41:35 -0600

    change api paths

--- src/lib/swagger.js
+++ src/lib/swagger.js
@@ -10,12 +10,7 @@ const options = {
       version: '1.0.0',
     },
   },
-  apis: [
-    path.resolve(process.cwd(), 'src/app/api/comando/route.js'),
-    path.resolve(process.cwd(), 'src/app/api/reportar/route.js'),
-    path.resolve(process.cwd(), 'src/app/api/getnombre/route.js'),
-    path.resolve(process.cwd(), 'src/app/api/comando-customizado/route.js')
-  ],
+  apis: ['./src/app/api/**/*.js'],
 }
 
 export const swaggerSpec = swaggerJSDoc(options)
